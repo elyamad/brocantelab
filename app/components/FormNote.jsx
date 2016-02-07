fe631105997208e9eac8cb3ed49053ef143eb12d
Adil EL-YAMOUNI <adil.elyamouni@google.com>
2016-02-07 20:01:56 +0100

    self validation by custom components !

--- app/components/FormNote.jsx
+++ app/components/FormNote.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ButtonInput, Input } from 'react-bootstrap';
+
+/* React bootstrap components */
+import { Panel, Input, Button } from 'react-bootstrap';
+
+/* Custom components */
+import NoteInput from './NoteInput';
+import EmailInput from './EmailInput';
 
 var FormNote = React.createClass({
-  getInitialState(){
+  getInitialState() {
     return {
-      clicked : false
     }
   },
-  handleClick(){
-    this.setState({clicked: !this.state.clicked});
+  isValidNote(valid) {
+    console.log("Note :", valid);
+  },
+  isValidEmail(valid) {
+    console.log("Email :", valid);
+  },
+  onSubmitForm() {
+    console.log(this.state);
   },
   render() {
     return (
-      <form>
-        <Input type="text" bsStyle="success" label="Success" hasFeedback />
-        <Input type="text" bsStyle="warning" label="Warning" hasFeedback />
-        <Input type="text" bsStyle="error" label="Error" hasFeedback />
-      </form>
+      <Panel header="Evaluation">
+        <form className="form-horizontal">
+          <NoteInput isValid={this.isValidNote} />
+          <EmailInput isValid={this.isValidEmail} />
+          <Input type="textarea" label="Comment" placeholder="Put your comment"labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
+          <Button bsStyle="primary" disabled={this.state.disabled} onClick={this.onSubmitForm} className="pull-right"> Send </Button>
+        </form>
+
+      </Panel>
     );
   }
 });
